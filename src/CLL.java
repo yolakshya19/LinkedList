05a5d4ac063961f9d2d9e687e677dd2cdfe52e27
Lakshya Dhawan <lakshyad1902@gmail.com>
2024-05-21 13:42:57 +0530

    Added features

--- src/CLL.java
+++ src/CLL.java
@@ -1,2 +1,43 @@
 public class CLL {
+
+    private Node head;
+    private Node tail;
+
+    public CLL() {
+        this.head = null;
+        this.tail = null;
+    }
+
+    // function to insert node after tail
+    public void insertVal(int val){
+        Node node = new Node(val);
+        if(head == null){
+            head = node;
+            tail = node;
+            return;
+        }
+        tail.next = node;
+        tail = node;
+        node.next = head;
+    }
+
+    //function to display
+    public void display() {
+        if (head == null) return;
+        Node temp = head;
+        do {
+            System.out.print(temp.val + " -> ");
+            temp = temp.next;
+        } while (temp != head);
+        System.out.println("(head)");
+    }
+
+    private class Node{
+        int val;
+        Node next;
+
+        public Node(int val) {
+            this.val = val;
+        }
+    }
 }
